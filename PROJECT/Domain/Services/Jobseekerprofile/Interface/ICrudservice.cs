227dc32d1e527e8e2266c1a9ad73ed7b24d27281
Amal2262ms <amalkrishna2262@gmail.com>
2026-09-14 11:51:25 +0530

    JobSeekerProfile

---
+++ PROJECT/Domain/Services/Jobseekerprofile/Interface/ICrudservice.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Models;
+using Domain.Services.Jobseekerprofile.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Services.Jobseekerprofile.Interface
+{
+  public  interface ICrudservice
+    {
+        Task<Jobseekerprofiledto?> CreateProfile(
+          Jobseekerprofiledto dto,
+          Guid systemUserId);
+
+        Task<Jobseekerprofiledto?> UpdateProfile(
+            Guid id,
+            Jobseekerprofiledto dto);
+
+        Task<bool> DeleteProfile(Guid id);
+
+        Task<Jobseekerprofiledto?> GetProfileById(Guid id);
+
+        Task<List<Skill>> GetAllSkills();
+
+        Task<List<Qualification>> GetAllQualifications();
+
+        Task<List<Experience>> GetAllExperiences();
+
+        Task<List<Location>> GetAllLocations();
+
+    }
+}
